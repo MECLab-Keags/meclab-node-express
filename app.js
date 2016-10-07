84a9e1f4efcd07a2794a1ac99191366184333aca
MECLab-Keags <keagan.soakell-ho@clinician.io>
2016-10-08 12:51:23 +1300

    initial project commit

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const express = require('express');
 
-
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const env = process.env.NODE_ENV || 'development';
+const bookRouter = express.Router();
+
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env === 'development';
+
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.render('index', {
+    nav: [
+      {link: '/books', text: 'Books'},
+      {link: '/authors', text: 'Authors'}]
+  });
 });
 
-app.get('/books', (req, res) => {
-  res.send('hello books');
-});
-
-
+const books = [
+  {title: 'Reactive Microservices Architecture', author: 'Jonas Boneir'},
+  {title: 'F# for Fun and Profit', author: 'Scott Wlaschin'}
+];
+bookRouter.route('/')
+          .get((req, res) => {
+            res.render('books', {
+              nav: [
+                {link: '/books', text: 'Books'},
+                {link: '/authors', text: 'Authors'}]
+            });
+          });
+bookRouter.route('/single')
+          .get((req, res) => {
+            res.send('hello single book');
+          });
+app.use('/books', bookRouter);
 app.listen(port, err => console.log('running server on port ' + port));
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,64 @@
 const gulp = require('gulp');
 const jshint = require('gulp-jshint');
 const jscs = require('gulp-jscs');
+const livereload = require('gulp-livereload');
+const server = require('gulp-develop-server');
 
 const package = require('./package');
 const config = package.jshintConfig;
-
-
 const jsFiles = ['*.js', 'src/**/*.js'];
+
 gulp.task('style', () => {
   return gulp.src(jsFiles)
              .pipe(jshint(config))
              .pipe(jshint.reporter('jshint-stylish', { verbose: true }))
              .pipe(jscs());
 });
+
+gulp.task('inject', () => {
+  const wiredep = require('wiredep').stream;
+  const options = {
+    bowerJson: require('./bower.json'),
+    directory: './public/lib',
+    ignorePath: '../../public'
+  };
+
+  const inject = require('gulp-inject');
+  const injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {read:false});
+  const injectOpt = {ignorePath: '/public'};
+
+  return gulp.src('./src/views/*.ejs')
+             .pipe(wiredep(options))
+             .pipe(inject(injectSrc, injectOpt))
+             .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('server:start', () => {
+  const options = {
+    path: './app.js',
+    delay: 600,
+    env: { 'PORT': 3000 }
+  };
+  server.listen(options, livereload.listen);
+});
+
+gulp.task('watch', ['server:start'], () => {
+  const js = ['*.js', './public/**/*.js', './public/*.js'];
+  const views = ['./src/views/*.ejs'];
+  const css = ['./public/css/*.css'];
+  gulp.watch(views, ['inject']);
+
+  gulp.watch(js.concat(views).concat(css)).on('change', file => {
+    server.changed(err => {
+      if(err){
+        console.error(err);
+        return;
+      }
+
+      console.log('file changed: ' + file.path);
+      livereload.changed(file.path);
+    });
+  });
+});
+
+gulp.task('default', ['style', 'inject', 'watch']);
